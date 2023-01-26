bd046e990ea3c7ccc3cf24d8ce6c2be6751e56e5
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-26 18:35:46 -0300

    feat: user model

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+
+    display_name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    image: {
+      type: DataTypes.STRING,
+    },
+  })
+
+  return User
+};
