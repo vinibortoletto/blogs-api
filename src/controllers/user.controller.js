13e941680e56422facfa65980f370fec2395fbd0
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-27 17:27:41 -0300

    refactor: using try/catch

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,19 @@
 const { userService } = require('../services');
-const { BAD_REQUEST, CONFLICT, CREATED, INTERNAL_SERVER_ERROR } = require('../utils/statusCodes');
+const { BAD_REQUEST, CONFLICT, CREATED } = require('../utils/statusCodes');
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const { type, message } = await userService.login(email, password);
-  if (type) return res.status(BAD_REQUEST).json({ message });
-
-  res.status(200).json({ token: message });
+  try {
+    const { type, message } = await userService.login(email, password);
+    if (type) return res.status(BAD_REQUEST).json({ message });
+    res.status(200).json({ token: message });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const create = async (req, res) => {
+const create = async (req, res, next) => {
   const newUser = req.body;
 
   try {
@@ -18,7 +21,7 @@ const create = async (req, res) => {
     if (type) return res.status(CONFLICT).json({ message });
     return res.status(CREATED).json({ token: message });
   } catch (error) {
-    return res.status(INTERNAL_SERVER_ERROR).json('Internal Service Error');
+    next(error);
   }
 };
 
--- src/utils/statusCodes.js
+++ src/utils/statusCodes.js
@@ -3,5 +3,4 @@ module.exports = {
   CREATED: 201,
   BAD_REQUEST: 400,
   CONFLICT: 409,
-  INTERNAL_SERVER_ERROR: 500,
 };
