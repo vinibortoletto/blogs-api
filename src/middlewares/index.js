78d0ac1130911c7dea8b34dbe48e6bbf4e5edbe7
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-27 16:59:32 -0300

    feat: validate login middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
+const validateLogin = require('./validateLogin');
 const validateDisplayName = require('./validateDisplayName');
 const validateEmail = require('./validateEmail');
 const validatePassword = require('./validatePassword');
 
-module.exports = { 
+module.exports = {
+  validateLogin,
   validateDisplayName,
-   validateEmail,
-   validatePassword,
+  validateEmail,
+  validatePassword,
 };
