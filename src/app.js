cac32ccc32a2f473b2d67393f24ab48f744c6bb3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-27 14:37:43 -0300

    feat: create user route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 // ...
 const { userController } = require('./controllers');
+const middlewares = require('./middlewares');
 
 const app = express();
 
 app.use(express.json());
 app.post('/login', userController.login);
+app.post(
+  '/user',
+  middlewares.validateDisplayName,
+  middlewares.validateEmail,
+  middlewares.validatePassword,
+  userController.create,
+);
+
 // ...
 
 // É importante exportar a constante `app`,
