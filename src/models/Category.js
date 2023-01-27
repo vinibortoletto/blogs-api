ddc5cd3cec82f7075c1854d7cd2f921a4883c28c
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-27 18:30:33 -0300

    feat: category model

---
+++ src/models/Category.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+      tableName: 'categories',
+      underscored: true
+    }
+  );
+
+  return Category;
+};
